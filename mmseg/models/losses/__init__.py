48750795e829a3b67b2a53ba9fa417eeb10261b5
Bedrettin Çetinkaya <bdrttn@gmail.com>
2024-05-14 14:06:03 +0300

    Update __init__.py

--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -5,10 +5,7 @@ from .lovasz_loss import LovaszLoss
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 from .hed_loss import (HEDLoss, hed_loss)
 from .ap_loss import APLoss
-from .ar_loss import ARLoss
-from .ap_loss_t import APLossT 
 from .ap_loss_orig import APLoss_orig
-from .rs_loss import RSLoss
 from .rank_loss import RankLoss
 from .sort_loss import SortLoss
 from .balanced_mse import BMSE
@@ -16,6 +13,6 @@ from .balanced_mse import BMSE
 __all__ = [
     'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
     'mask_cross_entropy', 'CrossEntropyLoss', 'reduce_loss',
-    'weight_reduce_loss', 'weighted_loss', 'LovaszLoss','HEDLoss', 'hed_loss', 'APLoss', 'ARLoss', 'APLossT', 'APLoss_orig',
-    'RSLoss','RankLoss','SortLoss', 'BMSE'
+    'weight_reduce_loss', 'weighted_loss', 'LovaszLoss','HEDLoss', 'hed_loss', 'APLoss', , 'APLoss_orig',
+    'RankLoss','SortLoss', 'BMSE'
 ]
